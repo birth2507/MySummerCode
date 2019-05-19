758a7b6977cb6754f5d57fcb77230ddf1bfd60c4
birth2507 <43840071+birth2507@users.noreply.github.com>
2019-05-19 16:20:20 +0530

    Update Que06.java

--- Que06.java
+++ Que06.java
@@ -1,28 +1,75 @@
 import java.util.*;
-class removeDuplicate{
-	public ArrayList<String> remove(ArrayList<String> a){
-		ArrayList<String> anew = new ArrayList<String>();
-		for(String s : a){
-			if(!anew.contains(s)){
-				anew.add(s);
+class LinkedList{
+	node head = null;
+	class node{
+		String data;
+		node next;
+		node(String data){
+			this.data = data;
+			this.next = null;
+		}
+	}	
+	public void insert(String data){
+		node n = new node(data);
+		head = insertValue(n);
+	}
+	private node insertValue(node n){
+		if(head == null){
+			head = n;
+		}
+		else{
+			node p = head;
+			while(p.next!=null){
+				p = p.next;
+				//System.out.println(p.data);
+			}
+			p.next = n;
+		}
+	return head;
+	}
+	public void print(){
+		node p = head;
+		int count =0;
+		//System.out.println(head.data);
+		while(p!=null){
+			System.out.println(p.data);
+			p = p.next;
+			count++;
+		}
+	}
+	public void removeDuplicate(){
+		HashMap<String,Integer> h = new HashMap<String,Integer>();
+		node p = head,q = p;
+		while(p!=null){									// ->  n
+			if(h.containsKey(p.data)){					        // ->  1
+				q.next = p.next;						// Time Complexity of removeDuplicate method is O(n);	
+				p = q.next;
+			}
+			else
+			{	h.put(p.data,1);                                                // -> 1
+				q = p;        
+				p = p.next;
 			}
 		}
-		return anew;
 	}
 }
 class Que06{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
-		ArrayList<String> a = new ArrayList<String>();
-		System.out.println("Enter number of String Object:");
-		int n= sc.nextInt();
-		for(int i=0;i<n;i++)
-		{
-			String s = sc.next();
-			a.add(s);
+		LinkedList l = new LinkedList();
+		System.out.println("Enter String object values:");
+		char c;
+		do{
+			String data = sc.next();
+			l.insert(data);
+			System.out.println("Enter 'y' if you want to insert more string Object else 'n'");
+			c = sc.next().charAt(0);
 		}
-		removeDuplicate r = new removeDuplicate();
-		a = r.remove(a);
-		System.out.println(a);
+		while(c=='y');
+		System.out.println("Before removing duplicate values:");
+		l.print();
+		l.removeDuplicate();
+		System.out.println("After removing duplicate values:");
+		l.print();
 	}
 }
